c955790b02f4def7447e774de5d2fba459aec4b3
Rafael Santos <rafael.santos@md2.local>
2021-08-11 08:11:46 -0300

    implementando a busca de todos os registros de person

--- 03 - person/src/main/java/br/com/erudio/service/PersonService.java
+++ 03 - person/src/main/java/br/com/erudio/service/PersonService.java
@@ -1,5 +1,7 @@
 package br.com.erudio.service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.stereotype.Service;
@@ -20,6 +22,27 @@ public class PersonService {
 		person.setGender("Male");
 		
 		return person;		
+	}
+	
+	public List<Person> findAll(){
+		List<Person> persons = new ArrayList<>();
+		
+		for (int i = 0; i < 8; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+		}
+		return persons;
+	}
+
+	private Person mockPerson(int i) {
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Person name " + i);
+		person.setLastName("Last name");
+		person.setAddress("Address" + i);
+		person.setGender("Male");
+		
+		return person;	
 	}
 
 }
